3811703dccaf7305d7e45de74b202d16ba7786da
Lynne Merchant <lmerchant@ucsd.edu>
2019-12-06 14:40:22 -0800

    Create calculator program
    Calculate addition, subtraction, multiplication, and division of two numbers.
    Add doctests.

---
+++ ucsd-ext-put-final/calculator.py
@@ -0,0 +1,44 @@
+"""
+Calculator.py
+
+Calculates addition, subtraction, multiplication and division of two numbers
+"""
+
+def add(num1, num2):
+    """ Adds two numbers
+    >>> add(2,4)
+    6
+    """
+    return num1, num2
+    
+def sub(num1, num2):
+    """ 
+    Subtracts two numbers
+    >>> sub(2,4)
+    -2
+    """
+    return num1 - num2
+    
+def mul(num1, num2):
+    """
+    Multiplies two numbers
+    >>> mul(2,4)
+    8
+    """
+    return num1 * num2
+    
+def div(num1, num2):
+    """ 
+    Divides two numbers
+    >>> div(4,2)
+    2
+    
+    Raises zero division error
+    >>> div(4,0)
+    Traceback (most recent call last):
+    ...
+    ZeroDivisionError: division by zero
+    """
+    return num1/num2
+    
+        
