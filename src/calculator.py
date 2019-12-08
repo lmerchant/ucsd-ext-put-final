f2a3b45d65e5e902be484ab6cd860d15b1c122ad
Lynne Merchant <merchant.lynne@gmail.com>
2019-12-08 13:35:18 -0800

    Apply black to fix formating

--- src/calculator.py
+++ src/calculator.py
@@ -4,21 +4,24 @@ Calculator.py
 Calculates addition, subtraction, multiplication and division of two numbers
 """
 
+
 def add(num1, num2):
     """ Adds two numbers
     >>> add(2,4)
     6
     """
     return num1 + num2
-    
+
+
 def sub(num1, num2):
-    """ 
+    """
     Subtracts two numbers
     >>> sub(2,4)
     -2
     """
     return num1 - num2
-    
+
+
 def mul(num1, num2):
     """
     Multiplies two numbers
@@ -26,9 +29,10 @@ def mul(num1, num2):
     8
     """
     return num1 * num2
-    
+
+
 def div(num1, num2):
-    """ 
+    """
     Divides two numbers
     >>> div(4,2)
     2.0
@@ -39,6 +43,4 @@ def div(num1, num2):
     ...
     ZeroDivisionError: division by zero
     """
-    return num1/num2
-    
-        
+    return num1 / num2
