2eec0be0c3436c80558a263385bf85c56c3ade78
Lynne Merchant <lmerchant@ucsd.edu>
2019-12-07 17:43:58 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 
-long_description='Mathematical functions to increment and decrement numbers along 
-                  with calculator functions of add, subrtract, multiply and divide'
+long_description='Mathematical functions to increment and decrement numbers along '\
+                 'with calculator functions of add, subrtract, multiply and divide'
 
 setup(
     name='UCSD Extension Put Final',
